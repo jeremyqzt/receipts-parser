30debb1c39b9c0bd58b4e35949b7103b64ba5895
Jeremy Qian <jq357@hotmail.com>
2022-12-27 22:00:37 -0500

    more boilerplate

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import pytesseract
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/index')
-def hello():
-    return 'Hello, World!'
+@app.route('/parse', methods=['GET', 'POST'])
+def acceptFile():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return "OK"
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=5001)
